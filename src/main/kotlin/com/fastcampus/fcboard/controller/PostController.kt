ea56e99bd11d33e93b58026400cf7684002d0bf6
gwanhyun.lee <anityche@gmail.com>
2024-05-22 21:32:56 +0900

    feature: get post test code

--- src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
+++ src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
@@ -1,8 +1,11 @@
 package com.fastcampus.fcboard.controller
 
 import com.fastcampus.fcboard.controller.dto.PostCreateRequest
+import com.fastcampus.fcboard.controller.dto.PostDetailResponse
 import com.fastcampus.fcboard.controller.dto.PostUpdateRequest
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @RestController
 class PostController {
@@ -29,5 +32,12 @@ class PostController {
     ): Long {
         println(createdBy)
         return id
+    }
+
+    @GetMapping("/posts/{id}")
+    fun getPost(
+        @PathVariable id: Long,
+    ): PostDetailResponse {
+        return PostDetailResponse(1L, "title", "content", "createdBy", LocalDateTime.now().toString())
     }
 }
